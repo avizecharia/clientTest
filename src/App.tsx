73da9c628c06d16dd8c4e972f696305532277c5f
aviel <aviel469@gmail.com>
2024-11-14 12:22:05 +0200

    add socket io

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router'
 import Register from './compomnets/auth/Register'
 import Login from './compomnets/auth/Login'
 import Page from './compomnets/pages/Page'
-import Nav from './compomnets/nav'
+import Nav from './compomnets/Nav'
+import { RootState, useAppDispatch, useAppSelector } from './redux/strore'
+import { socket } from './main'
+import { fetchGetDefenceAttack, fetchGetLaunche } from './redux/slice/launche'
 
 export default function App() {
+  const dis = useAppDispatch()
+  const {user} = useAppSelector((state:RootState) => state.user)
+  // useEffect(()=>{
+  //   socket.on("newLaunchHasOccurred",()=>{
+  //     if(user?.area){
+  //        dis(fetchGetDefenceAttack(user.area))
+  //     }
+  //     else{
+
+  //       dis(fetchGetLaunche(user?._id!))
+  //     }
+  //     // toast.info("Someone somewhere just voted");
+  //   })
+
+  // },[])
   return (
     <div>
       <Nav/>
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import App from "./App.tsx";
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 import store from "./redux/strore.ts";
+import { io } from 'socket.io-client'
+
+export const socket = io('http://localhost:7770')
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
