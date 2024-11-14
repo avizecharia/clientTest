45ae81edb6fef285538057de27e60fde4fafcba6
aviel <aviel469@gmail.com>
2024-11-14 12:22:24 +0200

    delete fefence route

--- src/compomnets/nav.tsx
+++ src/compomnets/nav.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { RootState, useAppDispatch, useAppSelector } from '../redux/strore'
 
-export default function nav() {
+export default function Nav() {
     const {user} = useAppSelector((state:RootState) => state.user)
   return (
-    <div><div className='nav'>
+    <div><div className='nav'>nav
     {user ? (<>
-    <NavLink to={"/defence"} >defence</NavLink>
     {/* <button onClick={handelLogout}>logout</button> */}
     </>):<>
     <NavLink to={"/login"} >Login</NavLink>
